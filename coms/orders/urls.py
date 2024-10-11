3dfb16abe539e12049013df452faa7128dc77c3f
Erick Owino Odera <118165396+Eodera1@users.noreply.github.com>
2024-10-11 08:57:00 +0300

    Update urls.py

--- coms/orders/urls.py
+++ coms/orders/urls.py
@@ -1,18 +1,3 @@
-
-# from django.urls import path, include
-# from rest_framework.routers import DefaultRouter
-# from orders.views import CustomerViewSet, OrderViewSet
-
-# router = DefaultRouter()
-# router.register(r'customers', CustomerViewSet)
-# router.register(r'orders', OrderViewSet)
-
-# urlpatterns = [
-#     path('', include(router.urls)),
-# ]
-
-# orders/urls.py
-
 from django.urls import path, include
 from .views import CustomerViewSet, OrderViewSet
 
